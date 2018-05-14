34a796d09f36ec53b6d95dad7a16db3096c9b2d3
dlemas <djlemas@ufl.edu>
2018-05-13 21:58:03 -0400

    facebook

--- getData_facebook.R
+++ getData_facebook.R
@@ -37,12 +37,12 @@ library(dplyr)
 library(ggplot2)
 
 # **************************************************************************** #
-# ***************       BEACH Report 3-23-18 to 4-13-18.xlsx                                              
+# ***************       BEACH Report 3-23-18 to 5-9-18 .xlsx                                              
 # **************************************************************************** #      
 
 # file parameters
 n_max=10000
-data.file.name="BEACH Report 3-23-18 to 4-13-18.xlsx";data.file.name
+data.file.name="BEACH Report 3-23-18 to 5-9-18 .xlsx";data.file.name
 
 # **************************************************************************** #
 # ***************                Results by Day of Week                                              
@@ -57,12 +57,23 @@ dat=day.dat
 names(dat)
 
 # rename
-newdata=rename(dat, reporting_starts = `Reporting Starts`, reporting_ends=`Reporting Ends`, ad_set_name='Ad Set Name', 
-               delivery=Delivery, results=`Results`, result_indicator=`Result Indicator`, reach=Reach,
-               impressions=Impressions, cost_per_results='Cost per Results', budget=Budget,
-               budget_type='Budget Type', amount_spent_USD='Amount Spent (USD)', ends=Ends,
-               frequency=Frequency, unique_link_clicks='Unique Link Clicks',
-               button_clicks='Button Clicks');newdata; names(newdata)
+newdata=rename(dat, reporting_starts = `Reporting Starts`, 
+               reporting_ends=`Reporting Ends`, 
+               ad_set_name='Ad Set Name', 
+               ad_set_delivery='Ad Set Delivery', 
+               results=`Results`, 
+               result_indicator='Result Indicator', 
+               reach=Reach,
+               impressions=Impressions, 
+               cost_per_results='Cost per Results', 
+               ad_set_budget='Ad Set Budget',
+               budget_type='Ad Set Budget Type', 
+               amount_spent_USD='Amount Spent (USD)', 
+               ends=Ends,
+               starts=Starts,
+               frequency=Frequency, 
+               unique_link_clicks='Unique Link Clicks',
+               button_clicks='Link Clicks');newdata; names(newdata)
 
 # compute day of week
 newdata$weekday=weekdays(as.Date(newdata$reporting_ends,'%Y-%m-%d',tz = "UTC"))
@@ -76,14 +87,63 @@ dat.days=newdata %>%
             cost.m=mean(cost_per_results)) %>%
   mutate(weekday.c = factor(weekday, levels = c("Sunday","Monday","Tuesday","Wednesday",
                                                 "Thursday","Friday","Saturday")))
-# plot: click.m 
-ggplot(dat.days, aes(x=factor(weekday.c), y=click.m))+geom_bar(stat="identity")
+# # plot: click.m 
+# ggplot(dat.days, aes(x=factor(weekday.c), y=click.m))+geom_bar(stat="identity")
+# 
+# # plot: button.m 
+# ggplot(dat.days, aes(x=factor(weekday.c), y=button.m))+geom_bar(stat="identity")
+# 
+# # plot: cost.m 
+# ggplot(dat.days, aes(x=factor(weekday.c), y=cost.m))+geom_bar(stat="identity")
+# 
 
-# plot: button.m 
-ggplot(dat.days, aes(x=factor(weekday.c), y=button.m))+geom_bar(stat="identity")
+# **************************************************************************** #
+# ***************                Results by Time of Day                                             
+# **************************************************************************** #
 
-# plot: cost.m 
-ggplot(dat.days, aes(x=factor(weekday.c), y=cost.m))+geom_bar(stat="identity")
+# read data
+time.dat=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "Results by Time of Day", range = NULL, col_names = TRUE,
+                  col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf,
+                  guess_max = min(1000, n_max));time.dat
 
+# data
+dat=time.dat
+names(dat)
 
+# rename
+newdata2=rename(dat, reporting_starts = `Reporting Starts`, 
+               reporting_ends=`Reporting Ends`, 
+               ad_set_name='Ad Set Name', 
+               time_of_day=`Time of Day (Ad Account Time Zone)`,
+               ad_set_delivery=`Ad Set Delivery`, 
+               results=`Results`, 
+               result_indicator='Result Indicator', 
+               reach=Reach,
+               impressions=Impressions, 
+               cost_per_results='Cost per Results', 
+               ad_set_budget='Ad Set Budget',
+               budget_type='Ad Set Budget Type', 
+               amount_spent_USD='Amount Spent (USD)', 
+               ends=Ends,
+               starts=Starts,
+               frequency=Frequency, 
+               unique_link_clicks='Unique Link Clicks',
+               link_clicks='Link Clicks',
+               button_clicks='Button Clicks');newdata2; names(newdata2)
 
+# compute 24 hours in day
+newdata2$hours=as.factor(seq_along(newdata2$time_of_day))
+range(newdata2$hours)
+names(newdata2)
+
+# # plot: click.m 
+# ggplot(newdata2, aes(x=factor(hours), y=results))+geom_bar(stat="identity")
+
+# **************************************************************************** #
+# ***************                Results by Age                                             
+# **************************************************************************** #
+
+# read data
+age.dat=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "Results by Age", range = NULL, col_names = TRUE,
+                   col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf,
+                   guess_max = min(1000, n_max));age.dat
